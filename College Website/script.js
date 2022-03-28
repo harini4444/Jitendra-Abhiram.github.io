a25a6f3bd2ab67a91c15d4199ec7da3d03761061
Jitendra Abhiram Chunduru <lcb2021014@iiitl.ac.in>
2022-03-28 16:43:25 +0530

    Publishing

---
+++ College Website/script.js
@@ -0,0 +1,9 @@
+var bar = document.getElementById("nav-links");
+function showMenu() {
+    bar.style.right = "0px";
+    console.log("it worked");
+}
+function hideMenu(){
+    bar.style.right = "-200px";
+}
+console.log(bar);
